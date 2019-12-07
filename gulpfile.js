def6a172ba08b0e176af97b69836a9c1a9a5be8b
smeggins <phillipchadwick@hotmail.com>
2019-12-07 14:22:39 -0800

    project fully initialized

--- gulpfile.js
+++ gulpfile.js
@@ -4,42 +4,51 @@ var gulp = require('gulp'),
     sass = require('gulp-sass'),
     autoprefixer = require('gulp-autoprefixer'),
     browserSync = require('browser-sync');
-​
+    cssnano = require('gulp-cssnano')
+    terser = require('gulp-terser');
+    rename = require('gulp-rename');
+
 var plumberErrorHandler = {
    errorHandler: notify.onError({
       title: 'Gulp',
       message: 'Error: <%= error.message %>'
    })
 };
-​
+
 gulp.task('sass', function() {
-   gulp.src('./sass/*.scss')
+   return gulp
+      .src('./sass/*.scss')
       .pipe(plumber(plumberErrorHandler))
       .pipe(sass())
       .pipe(autoprefixer({
          browsers: ['last 2 versions']
       }))
+      .pipe(cssnano())
+      .pipe(rename({extname: '.min.css'}))
       .pipe(gulp.dest('./build/css'))
 });
-​
+
 gulp.task('scripts', function(){
-    gulp.src('./js/*.js')
+    return gulp
+      .src('./js/*.js')
+      .pipe(terser())
+      .pipe(rename({extname: '.min.js'}))
       .pipe(gulp.dest('./build/js'))
 });
-​
+
 gulp.task('browser-sync', function() {
    browserSync.init({
       server: {
          baseDir: "./"
       }
    });
-​
+
    gulp.watch(['build/css/*.css', 'build/js/*.js']).on('change', browserSync.reload);
 });
-​
+
 gulp.task('watch', function() {
    gulp.watch('sass/*.scss', gulp.series('sass'));
    gulp.watch('js/*.js', gulp.series('scripts'));
 });
-​
+
 gulp.task('default', gulp.parallel('watch', 'browser-sync'));
